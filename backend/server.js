c07050c6c94b81628f2be62ad154c9056c9f67ba
AMix-hub <matztongring@gmail.com>
2025-10-03 17:25:01 +0200

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Backend fungerar på Raspberry Pi 🚀");
-});
-
-app.listen(3000, () => console.log("Server kör på http://localhost:3000"));
